a75aa199ec1817833536d24b743bd4493f851e87
Peter <schmidt.multimedia@gmail.com>
2021-05-26 14:41:10 +0200

    objekte / hierarchien

--- script_01.js
+++ script_01.js
@@ -11,7 +11,12 @@
 
 
 /***** Objekte 1 Daten/Funktionen *******/
-
+// ausgabe(person);
+// ausgabe(person.firstName);
+// ausgabe(person.familyName);
+// ausgabe(person.salary[0]);
+// ausgabe(person.hasAccess);
+// ausgabe(person.sayHello());
 let person = {
                 firstName:"Deniz",
                 familyName:"Yildiz",
@@ -24,18 +29,32 @@ let person = {
                         }
 };
 
-ausgabe(person);
-ausgabe(person.firstName);
-ausgabe(person.familyName);
-ausgabe(person.salary[0]);
-ausgabe(person.hasAccess);
-ausgabe(person.sayHello());
-
 let txt =  "Ich bin " + person.firstName + " " +
             person.familyName + " und verdiene " +
             person.salary[1] + " p.a.";
 
-ausgabe(txt);
+//ausgabe(txt);
+
+/***** Objekte 2 - Hierarchie *******/
+
+// Tiefer, tiefer - Irgendwo in der Tiefe gibt es ein Licht // K.Bush
+// Der Baikalsee ist mit 1642 m der tiefste See der Erde. ...
+// deep - deeper - deepest 
+// Nested Object
+
+let baikal = {
+                wert: "10m",
+                deep:{
+                        deeper:{
+                                deepest: "Das Licht - auf 1642m"
+                        }
+                    }
+};
+
+ausgabe(baikal);
+ausgabe(baikal.wert);
+ausgabe(baikal.deep.deeper.deepest);
+
 
 /** Ausgabe */
 function ausgabe(outputStr) {
